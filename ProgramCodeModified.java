337cd04b43a7eb19e3c78ad787f1ae88eeb2ec58
Maliarenko Anastasiia <162918839+xiaomaliarenko@users.noreply.github.com>
2025-11-15 20:53:00 +0200

    Create ProgramCodeModified.java

---
+++ ProgramCodeModified.java
@@ -0,0 +1,5 @@
+public class arthubResource { // Порушення: клас не final, назва не з великої літери
+public static void main(String[] args) { // Порушення: відсутні final та Javadoc
+System.out.println("Stworzono: Art-hub Resource!");
+}
+}
