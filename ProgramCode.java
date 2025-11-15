bc8494d1e0cfab03bfad256f88df271e0a636d1e
Maliarenko Anastasiia <162918839+xiaomaliarenko@users.noreply.github.com>
2025-11-15 20:15:11 +0200

    Update ProgramCode.java

--- ProgramCode.java
+++ ProgramCode.java
@@ -1,5 +1,13 @@
-public class ArtHubResource {
-    public static void main(String[] args) {
-        System.out.println("Stworeno: Art-hub Resource!");
+public final class ArtHubResource {
+
+    private ArtHubResource() {
+    }
+
+    /**
+     * Точка входу в програму. Виводить назву продукту.
+     * @param args Аргументи командного рядка
+     */
+    public static void main(final String[] args) {
+        System.out.println("Stworzono: Art-hub Resource!");
     }
 }
